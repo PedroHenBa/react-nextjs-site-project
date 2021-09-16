4cd9489fa0c89109450d597677af33364ad736d3
Pedro Henrique <pedroh.ba04@gmail.com>
2021-09-15 23:37:25 -0300

    Atualizando o readme.md

--- src/api/map-data.ts
+++ src/api/map-data.ts
@@ -3,7 +3,7 @@ import { mapSections } from './map-sections';
 import { PageData } from '../templates/Home';
 
 /* eslint-disable @typescript-eslint/no-explicit-any*/
-export const mapData = (pagesData = [{}] as any): PageData[] => {
+export const mapData = (pagesData = [{}]): PageData[] => {
   return pagesData.map((data: any): PageData => {
     const { footer_text: footerHtml = '', slug = '', title = '', menu = {}, sections = [] } = data;
     return {
